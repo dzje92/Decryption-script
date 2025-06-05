54e19a244aa1b34eebc9cf6a92f4ebbc9118029d
dzje92 <duongvanhongson710209@gmail.com>
2025-06-05 16:00:47 +0700

    Add files via upload

---
+++ 1.lua
@@ -0,0 +1,20 @@
+print("------------------------------------------------------------------------------------------------------------------------------------------------------------------------")
+local everything = {
+    "Loading...",
+    "test script decoding [ Part 1 ]",
+    "If there are enough [ 1 ] [ 2 ] [ 3 ] [ 4 ] or enough 4 🟢 then the best executor  ",
+    "If there are not enough [ 1 ] [ 2 ] [ 3 ] [ 4 ] or not enough 4 🟢 then the executor is not good  ",
+    "max [ 1 ] to [ 4 ] or ( 4 🟢 )",
+    " ",
+
+}
+
+for i, v in next, everything do
+    print(tostring(v))
+    task.wait()
+end
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/dzje92/dmhvip1/refs/heads/main/dmhlv1.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/dzje92/dmhvip1/refs/heads/main/dmhlv2.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/dzje92/dmhvip1/refs/heads/main/dmhlv3.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/dzje92/dmhvip1/refs/heads/main/dmhlv4.lua"))()
